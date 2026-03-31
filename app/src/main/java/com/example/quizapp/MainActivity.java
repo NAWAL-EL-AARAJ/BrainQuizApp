b4bb0291e4f6fed76950d117898e5d4cd36b26b2
Nawal El Aaraj <nawalelaaraje@gmail.com>
2026-03-31 17:46:16 +0200

    commit: adding JAVA code to main, register and quiz1.

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.quizapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etMail , etPassword;
+    Button btnLogin;
+    TextView tvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,29 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+            etMail = findViewById(R.id.Email);
+            etPassword = findViewById(R.id.password_toggle);
+            btnLogin = findViewById(R.id.signing);
+            tvRegister = findViewById(R.id.register);
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (etMail.getText().toString().equals("asmae") && etPassword.getText().toString().equals("2026")) {
+                        Intent i1 = new Intent(MainActivity.this, quiz1.class);
+                        startActivity(i1);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Wrong email or password", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+            tvRegister.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Intent i2 = new Intent(getApplicationContext(), Register.class);
+                    startActivity(i2);
+                }
+            });
         });
     }
 }
--- app/src/main/java/com/example/quizapp/Register.java
+++ app/src/main/java/com/example/quizapp/Register.java
@@ -1,6 +1,11 @@
 package com.example.quizapp;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Register extends AppCompatActivity {
+    EditText etName , etMail , etPassword , etConfpassword;
+    Button btnRegister ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,48 @@ public class Register extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        etName = findViewById(R.id.Name);
+        etMail = findViewById(R.id.Email);
+        etPassword = findViewById(R.id.password_toggle);
+        etConfpassword= findViewById(R.id.confirm_password_toggle);
+        btnRegister = findViewById(R.id.register_button);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = etMail.getText().toString();
+                String  password = etPassword.getText().toString();
+                String confpassword = etConfpassword.getText().toString();
+                if (TextUtils.isEmpty(mail)(
+                        Toast.makeText(Register.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                        return;
+            }
+            if (TextUtils.isEmpty(password)){
+                Toast.makeText(Register.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (TextUtils.isEmpty(confpassword)){
+                Toast.makeText(Register.this, "Please enter your confirm password", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (password.length()<6){
+                Toast.makeText(Register.this, "Password too short", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (!password.equals(confpassword)){
+                Toast.makeText(Register.this, "Password doesn't match", Toast.LENGTH_SHORT).show();
+                return;
+
+            }
+            Toast.makeText(Register.this, "Your account has been created", Toast.LENGTH_SHORT).show();
+
+
+
+                ))
+            }
+
+            }
+
+
+
     }
 }
